6eecf03972696ace10ff51ebf5c789a43e825bae
Paul  Trampert <paul.trampert@gmail.com>
2016-02-09 21:30:01 -0500

    Added Jws data structures.

---
+++ CertManager/Jws/Jws.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CertManager.Jws
+{
+    public class Jws<T>
+    {
+        public JwsHeader Protected { get; set; }
+        public dynamic Header { get; set; }
+        public T Payload { get; set; }
+        public string Signature { get; set; }
+    }
+}
---
+++ CertManager/Jws/JwsHeader.cs
@@ -0,0 +1,9 @@
+﻿namespace CertManager.Jws
+{
+    public class JwsHeader
+    {
+        public string Alg { get; set; }
+        public string Jwk { get; set; }
+        public string Nonce { get; set; }
+    }
+}
