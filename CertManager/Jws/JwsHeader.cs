8fad2161792e7e92694e3256ef9ee8bb42be3325
Paul  Trampert <paul.trampert@gmail.com>
2016-02-10 20:09:56 -0500

    Expand on the JwsHeader class.

--- CertManager/Jws/JwsHeader.cs
+++ CertManager/Jws/JwsHeader.cs
@@ -2,8 +2,59 @@
 {
     public class JwsHeader
     {
+        /// <summary>
+        /// Algorithm
+        /// </summary>
         public string Alg { get; set; }
+
+        /// <summary>
+        /// JWK Set Url
+        /// </summary>
+        public string Jku { get; set; }
+
+        /// <summary>
+        /// JSON Web Key
+        /// </summary>
         public string Jwk { get; set; }
-        public string Nonce { get; set; }
+
+        /// <summary>
+        /// Key ID
+        /// </summary>
+        public string Kid { get; set; }
+
+        /// <summary>
+        /// X.509 URL
+        /// </summary>
+        public string X5u { get; set; }
+
+        /// <summary>
+        /// X.509 Certificate Chain
+        /// </summary>
+        public string[] X5c { get; set; }
+
+        /// <summary>
+        /// X.509 Certificate SHA-1 Thumbprint
+        /// </summary>
+        public string X5t { get; set; }
+
+        /// <summary>
+        /// X.509 Certificate SHA-256 Thumbprint
+        /// </summary>
+        public string X5ts256 { get; set; }
+
+        /// <summary>
+        /// Type
+        /// </summary>
+        public string Typ { get; set; }
+
+        /// <summary>
+        /// Content Type
+        /// </summary>
+        public string Cty { get; set; }
+
+        /// <summary>
+        /// Critical
+        /// </summary>
+        public string Crit { get; set; }
     }
 }
