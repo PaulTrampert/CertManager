9352fb866f31204e130d4c9bb3792dbfb251f4be
Paul Trampert <paul.trampert@rackspace.com>
2016-02-11 11:52:09 -0500

    Added TestBase and AsyncTestBase.

---
+++ CertManager.Test/AsyncTestBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CertManager.Test
+{
+    
+    public abstract class AsyncTestBase
+    {
+        [SetUp]
+        public void Setup()
+        {
+            Context().Wait();
+            BecauseOf().Wait();
+        }
+
+        public abstract Task Context();
+
+        public abstract Task BecauseOf();
+    }
+}
---
+++ CertManager.Test/TestBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CertManager.Test
+{
+    
+    public abstract class TestBase
+    {
+        [SetUp]
+        public void Setup()
+        {
+            Context();
+            BecauseOf();
+        }
+
+        public abstract void Context();
+
+        public abstract void BecauseOf();
+    }
+}
