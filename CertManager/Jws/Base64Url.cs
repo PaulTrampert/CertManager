687cc20ca3200829dd231e89f0d059b5cb0d6fae
Paul  Trampert <paul.trampert@gmail.com>
2016-02-10 20:49:24 -0500

    Started implementing JWS creation.

---
+++ CertManager/Jws/Base64Url.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Text;
+
+namespace CertManager.Jws
+{
+    public class Base64Url
+    {
+        public static string ToBase64UrlString(string toEncode)
+        {
+            return ToBase64UrlString(Encoding.UTF8.GetBytes(toEncode));
+        }
+
+        public static string ToBase64UrlString(byte[] toEncode)
+        {
+            return Convert.ToBase64String(toEncode).Replace("=", "").Replace('+', '-').Replace('/', '_');
+        }
+    }
+}
---
+++ CertManager/Jws/JwsHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace CertManager.Jws
+{
+    public class JwsHandler
+    {
+        private JsonSerializerSettings JsonSettings { get; }
+
+        public JwsHandler()
+        {
+            JsonSettings = new JsonSerializerSettings();
+            JsonSettings.NullValueHandling = NullValueHandling.Ignore;
+            JsonSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            JsonSettings.DateFormatHandling = DateFormatHandling.IsoDateFormat;
+        }
+
+        public Jws CreateJws<T>(T obj)
+        {
+            var result = new Jws();
+            var jsonPayload = JsonConvert.SerializeObject(obj);
+            result.Payload = Base64Url.ToBase64UrlString(jsonPayload);
+            var header = new JwsHeader();
+            header.Alg = "HS256";
+            var hmacsha256 = new HMACSHA256();
+            header.Jwk = Base64Url.ToBase64UrlString(hmacsha256.Key);
+            result.Protected = Base64Url.ToBase64UrlString(JsonConvert.SerializeObject(header));
+            result.Signature = Base64Url.ToBase64UrlString(hmacsha256.ComputeHash(Encoding.UTF8.GetBytes($"{result.Protected}.{result.Payload}")));
+            return result;
+        }
+    }
+}
